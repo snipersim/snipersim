bee40469e77581cb6c65d309a5c2dac172bb310c
Wim Heirman <wim@heirman.net>
2012-09-28 16:06:09 +0200

    [PATCH 11/51] [addr2line] Look for SNIPER_ROOT as well as GRAPHITE_ROOT

--- tools/addr2line.py
+++ tools/addr2line.py
@@ -5,7 +5,7 @@ import sys, os, subprocess
 def ex(cmd):
   return subprocess.Popen([ 'bash', '-c', cmd ], stdout = subprocess.PIPE).communicate()[0]
 
-bin = os.path.join(os.getenv('GRAPHITE_ROOT'), 'lib', 'pin_sim.so')
+bin = os.path.join(os.getenv('SNIPER_ROOT') or os.getenv('GRAPHITE_ROOT'), 'lib', 'pin_sim.so')
 base = eval('0x' + ex('nm %s | grep _Z5rdtscv' % bin).split()[0])
 text = eval('0x' + ex(r'objdump -h %s | grep "\.text"' % bin).split()[5])
 
