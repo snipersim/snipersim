eadcc302fd3b82f85b344ca6286e9f945b7e4446
Wim Heirman <wim.heirman@elis.ugent.be>
2012-10-10 12:05:31 +0200

    [PATCH 50/51] [markers] Allow writing out of statistics dump on each magic marker by adding a command-line option (-smarkers:stats)

--- scripts/markers.py
+++ scripts/markers.py
@@ -2,13 +2,21 @@
 
 import sim
 class Marker:
+  def setup(self, args):
+    args = (args or '').split(',')
+    if 'stats' in args:
+      self.write_stats = True
+    else:
+      self.write_stats = True
   def hook_magic_marker(self, thread, core, a, b):
     print '[SCRIPT] Magic marker from thread %d: a = %d, b = %d' % (thread, a, b)
 
-    # Uncomment to write out statistics at each magic marker
+    # Pass in 'stats' as option to write out statistics at each magic marker
+    # $ run-sniper -s markers:stats
     # This will allow for e.g. partial CPI stacks of specific code regions:
     # $ tools/cpistack.py --partial=marker-1-4:marker-2-4
 
-    #sim.stats.write('marker-%d-%d' % (a, b))
+    if self.write_stats:
+      sim.stats.write('marker-%d-%d' % (a, b))
 
 sim.util.register(Marker())
