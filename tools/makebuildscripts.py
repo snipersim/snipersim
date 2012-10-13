d12ba7cb6b492c87218e28c908f2e58ee2ba9e65
Trevor Erik Carlson <Trevor.Carlson@elis.UGent.be>
2012-10-13 11:06:07 +0200

    [PATCH 18/85] [tools] Update the build script creator to supply both SNIPER_* as well as GRAPHITE_* variables

--- tools/makebuildscripts.py
+++ tools/makebuildscripts.py
@@ -6,7 +6,7 @@ import sys, os, subprocess
 sim_root, pin_home, cc, cxx, arch = sys.argv[1:]
 
 # Needed for sim_api.h
-includes = '-I${GRAPHITE_ROOT}/include'
+includes = '-I${SNIPER_ROOT}/include'
 
 if arch == 'intel64':
   arch_cflags = ''
@@ -28,32 +28,34 @@ def determine_valid_flags():
 
 extra_cflags = determine_valid_flags()
 
-flags = {
-  'GRAPHITE_CFLAGS': '%(extra_cflags)s %(includes)s %(arch_cflags)s' % locals(),
-  'GRAPHITE_CXXFLAGS': '%(extra_cflags)s %(includes)s %(arch_cflags)s' % locals(),
-  'GRAPHITE_LDFLAGS': '-static -L${GRAPHITE_ROOT}/lib -pthread %(arch_ldflags)s' % locals(),
-  'GRAPHITE_LD_LIBRARY_PATH': '',
-  'GRAPHITE_CC': cc,
-  'GRAPHITE_CXX': cxx,
-  'GRAPHITE_LD': cxx,
-  'PIN_HOME': pin_home,
-}
-upcc_link = ('%(GRAPHITE_CXX)s %(GRAPHITE_LDFLAGS)s' % flags).strip()
-flags['GRAPHITE_UPCCFLAGS'] = "%(includes)s %(arch_cflags)s -link-with='%(upcc_link)s'" % locals()
+flags = {}
+for f in ('SNIPER', 'GRAPHITE'):
+  flags['%s_CFLAGS'%f] = '%(extra_cflags)s %(includes)s %(arch_cflags)s' % locals()
+  flags['%s_CXXFLAGS'%f] = '%(extra_cflags)s %(includes)s %(arch_cflags)s' % locals()
+  flags['%s_LDFLAGS'%f] = '-static -L${SNIPER_ROOT}/lib -pthread %(arch_ldflags)s' % locals()
+  flags['%s_LD_LIBRARY_PATH'%f] = ''
+  flags['%s_CC'%f] = cc
+  flags['%s_CXX'%f] = cxx
+  flags['%s_LD'%f] = cxx
+flags['PIN_HOME'] = pin_home
+
+upcc_link = ('%(SNIPER_CXX)s %(SNIPER_LDFLAGS)s' % flags).strip()
+for f in ('SNIPER', 'GRAPHITE'):
+  flags['%s_UPCCFLAGS'%f] = "%(includes)s %(arch_cflags)s -link-with='%(upcc_link)s'" % locals()
 
 message = '# This file is auto-generated, changes made to it will be lost. Please edit %s instead.' % os.path.basename(__file__)
 
-env_check_sh = 'if [ -z "${GRAPHITE_ROOT}" ] ; then GRAPHITE_ROOT=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..") ; fi'
-env_check_make = 'SELF_DIR := $(dir $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))\nGRAPHITE_ROOT ?= $(shell readlink -f $(SELF_DIR)/..)'
+env_check_sh = 'if [ -z "${SNIPER_ROOT}" ] ; then SNIPER_ROOT=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..") ; fi'
+env_check_make = 'SELF_DIR := $(dir $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))\nSNIPER_ROOT ?= $(shell readlink -f $(SELF_DIR)/..)'
 
 file('config/buildconf.sh', 'w').write('\n'.join(
   [ message, '' ] +
   [ env_check_sh, '' ] +
-  [ '%s="%s"' % i for i in flags.items() ]
+  [ '%s="%s"' % i for i in sorted(flags.items(),key=lambda x:x[0]) ]
 ) + '\n')
 
 file('config/buildconf.makefile', 'w').write('\n'.join(
   [ message, '' ] +
   [ env_check_make, '' ] +
-  [ '%s:=%s' % i for i in flags.items() ]
+  [ '%s:=%s' % i for i in sorted(flags.items(),key=lambda x:x[0]) ]
 ) + '\n')
