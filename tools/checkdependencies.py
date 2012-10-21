7f564e3e99d0f58be05a23075b56dfbb6403fc32
Wim Heirman <wim@heirman.net>
2012-10-21 11:25:26 +0200

    [PATCH 53/85] [dependencies] Provide RedHat-style package name for BerkeleyDB library

--- tools/checkdependencies.py
+++ tools/checkdependencies.py
@@ -8,7 +8,7 @@ DEPENDENCIES = [
   ('zlib1g-dev / zlib-devel', '/usr/include/zlib.h'),
   ('g++ / gcc-c++', '/usr/bin/g++'),
   ('wget', '/usr/bin/wget'),
-  ('libdb-dev', '/usr/include/db.h'),
+  ('libdb-dev / db4-devel', '/usr/include/db.h'),
 ]
 
 if os.environ.get('BOOST_INCLUDE', ''):
