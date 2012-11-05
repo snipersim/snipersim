5a5cf9e8d1fad03a09643b5a45be69c16277c260
Wim Heirman <wim@heirman.net>
2012-11-05 11:59:44 +0100

    [PATCH 77/85] [scripts] Periodic stats script: support dumping statistics by (aggregate) instructions interval

--- scripts/periodic-stats.py
+++ scripts/periodic-stats.py
@@ -2,7 +2,8 @@
 periodic-stats.py
 
 Periodically write out all statistics
-Argument is the interval size in nanoseconds (default is 1e9 = 1 second of simulated time)
+1st argument is the interval size in nanoseconds (default is 1e9 = 1 second of simulated time)
+2nd argument, if "ins" then interval is number of instructions rather than nanoseconds
 """
 
 import sim
@@ -10,8 +11,12 @@ import sim
 class PeriodicStats:
   def setup(self, args):
     args = dict(enumerate((args or '').split(':')))
-    interval_ns = long(args.get(0, 1e9))
-    sim.util.Every(interval_ns * sim.util.Time.NS, self.periodic, roi_only = True)
+    interval = long(args.get(0, 1e9))
+    by_ins = args.get(1, '')
+    if by_ins == 'ins':
+      sim.util.EveryIns(interval, self.periodic, roi_only = True)
+    else:
+      sim.util.Every(interval * sim.util.Time.NS, self.periodic, roi_only = True)
 
   def periodic(self, time, time_delta):
     sim.stats.write('periodic-%d' % time)
