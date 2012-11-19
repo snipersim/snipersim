8e4a3a111914e709fa54bc2eb43701c348fd862f
Wim Heirman <wim@heirman.net>
2012-11-19 15:28:48 +0100

    [PATCH 26/47] [scripts] Expose statistics database to Python scripts as sim.stats.db

--- scripts/sim/__init__.py
+++ scripts/sim/__init__.py
@@ -5,3 +5,6 @@ import sim_dvfs as dvfs
 import sim_control as control
 import sim_bbv as bbv
 import util
+
+import os, sqlite3
+stats.db = sqlite3.connect(os.path.join(config.output_dir, 'sim.stats.sqlite3'))
