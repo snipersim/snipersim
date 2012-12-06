2d6eddebcfb15dcf0bceebae0b52ad9d4d53f7e7
Trevor Erik Carlson <Trevor.Carlson@elis.UGent.be>
2012-12-06 01:26:55 +0100

    [PATCH 105/123] [viz] Update the RangeSlider Rickshaw component to accept an Array of graphs to control

--- tools/viz/rickshaw/src/js/Rickshaw.Graph.RangeSlider.js
+++ tools/viz/rickshaw/src/js/Rickshaw.Graph.RangeSlider.js
@@ -3,49 +3,57 @@ Rickshaw.namespace('Rickshaw.Graph.RangeSlider');
 Rickshaw.Graph.RangeSlider = function(args) {
 
 	var element = this.element = args.element;
-	var graph = this.graph = args.graph;
+	var graph;
+
+        if (args.graph.constructor !== Array) {
+                graph = this.graph = new Array(args.graph);
+        } else {
+                graph = this.graph = args.graph;
+        }
 
 	$( function() {
 		$(element).slider( {
 
 			range: true,
-			min: graph.dataDomain()[0],
-			max: graph.dataDomain()[1],
+			min: graph[0].dataDomain()[0],
+			max: graph[0].dataDomain()[1],
 			values: [ 
-				graph.dataDomain()[0],
-				graph.dataDomain()[1]
+				graph[0].dataDomain()[0],
+				graph[0].dataDomain()[1]
 			],
 			slide: function( event, ui ) {
 
-				graph.window.xMin = ui.values[0];
-				graph.window.xMax = ui.values[1];
-				graph.update();
+				for (var i=0; i < graph.length; i++) {
+					graph[i].window.xMin = ui.values[0];
+					graph[i].window.xMax = ui.values[1];
+					graph[i].update();
 
-				// if we're at an extreme, stick there
-				if (graph.dataDomain()[0] == ui.values[0]) {
-					graph.window.xMin = undefined;
-				}
-				if (graph.dataDomain()[1] == ui.values[1]) {
-					graph.window.xMax = undefined;
+					// if we're at an extreme, stick there
+					if (graph[i].dataDomain()[0] == ui.values[0]) {
+						graph[i].window.xMin = undefined;
+					}
+					if (graph[i].dataDomain()[1] == ui.values[1]) {
+						graph[i].window.xMax = undefined;
+					}
 				}
 			}
 		} );
 	} );
 
-	element[0].style.width = graph.width + 'px';
+	element[0].style.width = graph[0].width + 'px';
 
-	graph.onUpdate( function() {
+	graph[0].onUpdate( function() {
 
 		var values = $(element).slider('option', 'values');
 
-		$(element).slider('option', 'min', graph.dataDomain()[0]);
-		$(element).slider('option', 'max', graph.dataDomain()[1]);
+		$(element).slider('option', 'min', graph[0].dataDomain()[0]);
+		$(element).slider('option', 'max', graph[0].dataDomain()[1]);
 
-		if (graph.window.xMin == undefined) {
-			values[0] = graph.dataDomain()[0];
+		if (graph[0].window.xMin == undefined) {
+			values[0] = graph[0].dataDomain()[0];
 		}
-		if (graph.window.xMax == undefined) {
-			values[1] = graph.dataDomain()[1];
+		if (graph[0].window.xMax == undefined) {
+			values[1] = graph[0].dataDomain()[1];
 		}
 
 		$(element).slider('option', 'values', values);
