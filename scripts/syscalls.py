a9805d34ed0d535cd26b1a754651d7ef0fc12a28
Wim Heirman <wim@heirman.net>
2012-12-11 21:23:59 +0100

    [scripts] Add syscall strings for Python

--- scripts/syscalls.py
+++ scripts/syscalls.py
@@ -1,10 +1,18 @@
-import sim
+import sim, syscall_strings, platform
+
+if platform.architecture()[0] == '64bit':
+  __syscall_strings = syscall_strings.syscall_strings_64
+else:
+  __syscall_strings = syscall_strings.syscall_strings_32
+
+def syscall_name(syscall_number):
+  return '%s[%d]' % (__syscall_strings.get(syscall_number, 'unknown'), syscall_number)
 
 class LogSyscalls:
   def hook_syscall_enter(self, threadid, coreid, time, syscall_number, args):
-    print '[SYSCALL] @%d ns: thread(%d) core(%d) syscall(%d) args(%s)' % (time/1e6, threadid, coreid, syscall_number, args)
+    print '[SYSCALL] @%10d ns: %-27s thread(%3d) core(%3d) args%s' % (time/1e6, syscall_name(syscall_number), threadid, coreid, args)
 
   def hook_syscall_exit(self, threadid, coreid, time, ret_val, emulated):
-    print '[SYSCALL] @%d ns: thread(%d) core(%d) syscall exit ret_val(%d) emulated(%s)' % (time/1e6, threadid, coreid, ret_val, emulated)
+    print '[SYSCALL] @%10d ns:                        exit thread(%3d) core(%3d) ret_val(%d) emulated(%s)' % (time/1e6, threadid, coreid, ret_val, emulated)
 
 sim.util.register(LogSyscalls())
