fa90d0e91800ac696c9d0749e63b31a7226ef5f9
Trevor Erik Carlson <Trevor.Carlson@elis.UGent.be>
2012-12-11 16:32:13 +0100

    [tools] Pass CentOS as ScientificLinux as they are both RedHat derivatives

--- tools/get_os_type.sh
+++ tools/get_os_type.sh
@@ -9,6 +9,12 @@ if [ "$RES" -eq 0 ]; then
 	OS_TYPE="ScientificLinux"
 fi
 
+grep -q "CentOS" /etc/issue >/dev/null 2>&1
+RES="$?"
+if [ "$RES" -eq 0 ]; then
+	OS_TYPE="ScientificLinux"
+fi
+
 grep -q "Ubuntu" /etc/issue >/dev/null 2>&1
 RES="$?"
 if [ "$RES" -eq 0 ]; then
