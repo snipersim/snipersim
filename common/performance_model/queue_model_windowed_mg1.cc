afa6e6c6c191e7782ab8a616c3456c12ceaf6a38
Wim Heirman <wim@heirman.net>
2013-03-25 15:51:18 +0100

    [windowed_mg1] Add usage/delay statistics

--- common/performance_model/queue_model_windowed_mg1.cc
+++ common/performance_model/queue_model_windowed_mg1.cc
@@ -2,13 +2,18 @@
 #include "simulator.h"
 #include "config.hpp"
 #include "log.h"
+#include "stats.h"
 
 QueueModelWindowedMG1::QueueModelWindowedMG1(String name, UInt32 id)
    : m_window_size(SubsecondTime::NS(Sim()->getCfg()->getInt("queue_model/windowed_mg1/window_size")))
    , m_num_arrivals(0)
    , m_service_time_sum(0)
    , m_service_time_sum2(0)
-{}
+{
+   registerStatsMetric(name, id, "num-requests", &m_total_requests);
+   registerStatsMetric(name, id, "total-time-used", &m_total_utilized_time);
+   registerStatsMetric(name, id, "total-queue-delay", &m_total_queue_delay);
+}
 
 QueueModelWindowedMG1::~QueueModelWindowedMG1()
 {}
@@ -39,6 +44,10 @@ QueueModelWindowedMG1::computeQueueDelay(SubsecondTime pkt_time, SubsecondTime p
    }
 
    addItem(pkt_time, processing_time);
+
+   m_total_requests++;
+   m_total_utilized_time += processing_time;
+   m_total_queue_delay += t_queue;
 
    return t_queue;
 }
--- common/performance_model/queue_model_windowed_mg1.h
+++ common/performance_model/queue_model_windowed_mg1.h
@@ -18,6 +18,10 @@ public:
 private:
    const SubsecondTime m_window_size;
 
+   UInt64 m_total_requests;
+   SubsecondTime m_total_utilized_time;
+   SubsecondTime m_total_queue_delay;
+
    std::multimap<SubsecondTime, SubsecondTime> m_window;
    UInt64 m_num_arrivals;
    UInt64 m_service_time_sum; // In ps
