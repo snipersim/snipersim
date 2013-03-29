f9f0476286cd8d3cfd37e1361f8355d998d32118
Wim Heirman <wim@heirman.net>
2013-03-29 16:09:42 +0100

    [gen_topology] Fallback when topology information cannot be read (old-format simulations) to still show the 2-D mesh utilization, fix mesh plot for jobid-based runs, dynamically determine SVG size

--- tools/gen_topology.py
+++ tools/gen_topology.py
@@ -10,13 +10,21 @@ def gen_topology(resultsdir = '.', jobid = None, outputobj = sys.stdout, format 
   stats = sniper_stats.SniperStats(resultsdir, jobid)
   config = sniper_lib.get_config(resultsdir = resultsdir, jobid = jobid)
 
+  try:
+    topology = stats.get_topology()
+  except:
+    print >> sys.stderr, "Failed getting topology information"
+    topology = None
+
+
   max_id = 0
-  for name, lid, mid in stats.get_topology():
-    if name not in names:
-      print >> sys.stderr, 'Unknown component', name
-      continue
-    ids[name][int(lid)] = int(mid)
-    max_id = max(max_id, int(lid))
+  if topology:
+    for name, lid, mid in stats.get_topology():
+      if name not in names:
+        print >> sys.stderr, 'Unknown component', name
+        continue
+      ids[name][int(lid)] = int(mid)
+      max_id = max(max_id, int(lid))
 
 
   def format_config(name, lid):
@@ -53,81 +61,89 @@ def gen_topology(resultsdir = '.', jobid = None, outputobj = sys.stdout, format 
 
 
   elif format == 'svg':
-    margin_x = 50; step_x = 110
-    margin_y = 50; step_y = 50
-    items = []
-    def paint_init(w, h):
-      if not embedded:
-        print >> outputobj, '''\
+    class Svg:
+      def __init__(self):
+        self.margin_x = 50; self.step_x = 110
+        self.margin_y = 50; self.step_y = 50
+        self.size_x = 0; self.size_y = 0
+        self.items = []
+      def paint_box(self, (x, y), (w, h), name = '', label = 0, color = '#ffffff', zorder = 0, margin = (.2, .2)):
+        self.size_x = max(self.size_x, (x+w) * self.step_x); self.size_y = max(self.size_y, (y+h) * self.step_y)
+        svg = '''\
+<rect id="%s" x="%d" y="%d" width="%d" height="%d" rx="0"
+   style="stroke:#000000;stroke-width:1;stroke-linejoin:miter; stroke-linecap:butt;fill:%s;"/>
+  ''' % (name, self.margin_x + x * self.step_x, self.margin_y + y * self.step_y,
+               (w - margin[0]) * self.step_x, (h - margin[1]) * self.step_y, color)
+        if label:
+          svg += '''\
+<text xml:space="preserve" x="%d" y="%d" fill="#000000"  font-family="Times" font-style="normal" font-weight="normal" font-size="12" text-anchor="start">%s</text>
+  ''' % (self.margin_x + (x + .1) * self.step_x, self.margin_y + (y + .3) * self.step_y, label)
+        self.items.append((zorder, svg))
+      def write(self, outputobj):
+        if not embedded:
+          print >> outputobj, '''\
 <?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 '''
-      print >> outputobj, '''\
+        print >> outputobj, '''\
 <svg xmlns="http://www.w3.org/2000/svg" width="%d" height="%d">
 <g style="stroke-width:.025in; fill:none">
-  ''' % (2*margin_x + w * step_x, 2*margin_y + h * step_y)
-    def paint_box((x, y), (w, h), name = '', label = 0, color = '#ffffff', zorder = 0, margin = (.2, .2)):
-      svg = '''\
-<rect id="%s" x="%d" y="%d" width="%d" height="%d" rx="0"
-   style="stroke:#000000;stroke-width:1;stroke-linejoin:miter; stroke-linecap:butt;fill:%s;"/>
-  ''' % (name, margin_x + x * step_x, margin_y + y * step_y, (w - margin[0]) * step_x, (h - margin[1]) * step_y, color)
-      if label:
-        svg += '''\
-<text xml:space="preserve" x="%d" y="%d" fill="#000000"  font-family="Times" font-style="normal" font-weight="normal" font-size="12" text-anchor="start">%s</text>
-  ''' % (margin_x + (x + .1) * step_x, margin_y + (y + .3) * step_y, label)
-      items.append((zorder, svg))
-    def paint_fini():
-      for order, svg in sorted(items, reverse = True):
-        print >> outputobj, svg
-      print >> outputobj, '''\
+  ''' % (self.size_x + 2*self.margin_x, self.size_y + 2*self.margin_y)
+        for order, svg in sorted(self.items, reverse = True):
+          print >> outputobj, svg
+        print >> outputobj, '''\
 </g>
 </svg>
   '''
 
-    paint_init(max_id+3, len(names)+1)
-    xpos = range(max_id+1)
-    if ids['smt'].keys():
-      x = -1
-      for lid in range(max_id+1):
-        if ids['smt'][lid] == lid:
-          x += 1
-        xpos[lid] = x
-      ypos = [ 0 for _ in range(max_id+1) ]
-      for lid in range(max_id+1):
-        mid = ids['smt'][lid]
-        paint_box((xpos[lid]+.05, ypos[mid]+.1), (1-.1, 1-.2), 'core-%d' % lid, 'Core #%d' % lid)
-        ypos[mid] += .7
-      for lid in range(max_id+1):
-        if ids['smt'][lid] == lid:
-          paint_box((xpos[lid], 0), (1, ypos[mid] + .3), 'smt-%d' % lid, color = '#cccccc', zorder = 1)
-      y = max(ypos) + .3
+    svg = Svg()
+
+    if topology:
+      xpos = range(max_id+1)
+      if ids['smt'].keys():
+        x = -1
+        for lid in range(max_id+1):
+          if ids['smt'][lid] == lid:
+            x += 1
+          xpos[lid] = x
+        ypos = [ 0 for _ in range(max_id+1) ]
+        for lid in range(max_id+1):
+          mid = ids['smt'][lid]
+          svg.paint_box((xpos[lid]+.05, ypos[mid]+.1), (1-.1, 1-.2), 'core-%d' % lid, 'Core #%d' % lid)
+          ypos[mid] += .7
+        for lid in range(max_id+1):
+          if ids['smt'][lid] == lid:
+            svg.paint_box((xpos[lid], 0), (1, ypos[mid] + .3), 'smt-%d' % lid, color = '#cccccc', zorder = 1)
+        y = max(ypos) + .3
+      else:
+        for lid in range(max_id+1):
+          svg.paint_box((lid, 0), (1, 1), 'core-%d' % lid, 'Core #%d' % lid)
+        y = 1
+      for name in names:
+        if name in ('hwcontext', 'smt'): continue
+        if ids[name]:
+          size = 0
+          for lid in range(max_id, -1, -1):
+            if not ids['smt'] or ids['smt'][lid] == lid:
+              size += 1
+            if ids[name][lid] == lid:
+              cfg = format_config(name, lid)
+              if cfg:
+                label = '%s (%s)' % (name, cfg)
+              else:
+                label = name
+              svg.paint_box((xpos[lid], y), (size, 1), '%s-%d' % (name, lid), label)
+              if name == 'dram-cntlr':
+                svg.paint_box((xpos[lid]-.075, -.2), (size+.15, y+1+.4), color = '#dddddd', zorder = 2)
+              size = 0
+          y += 1
     else:
-      for lid in range(max_id+1):
-        paint_box((lid, 0), (1, 1), 'core-%d' % lid, 'Core #%d' % lid)
-      y = 1
-    for name in names:
-      if name in ('hwcontext', 'smt'): continue
-      if ids[name]:
-        size = 0
-        for lid in range(max_id, -1, -1):
-          if not ids['smt'] or ids['smt'][lid] == lid:
-            size += 1
-          if ids[name][lid] == lid:
-            cfg = format_config(name, lid)
-            if cfg:
-              label = '%s (%s)' % (name, cfg)
-            else:
-              label = name
-            paint_box((xpos[lid], y), (size, 1), '%s-%d' % (name, lid), label)
-            if name == 'dram-cntlr':
-              paint_box((xpos[lid]-.075, -.2), (size+.15, y+1+.4), color = '#dddddd', zorder = 2)
-            size = 0
-        y += 1
+      y = 0
 
 
     if sniper_config.get_config(config, 'network/memory_model_1') == 'emesh_hop_by_hop':
 
-      results = sniper_lib.get_results(resultsdir = resultsdir)['results']
+      results = sniper_lib.get_results(resultsdir = resultsdir, jobid = jobid)['results']
       if 'dram-queue.total-time-used' in results \
          and 'network.shmem-1.mesh.link-up.num-requests' in results:
         import gridcolors
@@ -153,7 +169,7 @@ def gen_topology(resultsdir = '.', jobid = None, outputobj = sys.stdout, format 
               if c > 0:
                 continue
               core = (y * width + x) * concentration + c
-              paint_box((x*SCALE_X, y+OFFSET_Y), (SCALE_X*.8, 1*.8), 'node-%d-%d' % (x, y), margin = (0, 0))
+              svg.paint_box((x*SCALE_X, y+OFFSET_Y), (SCALE_X*.8, 1*.8), 'node-%d-%d' % (x, y), margin = (0, 0))
               for link, _x, _y in (
                 ('down', 0, -.5), ('up', 0, .5),
                 ('left', -.5, 0), ('right', .5, 0),
@@ -163,15 +179,16 @@ def gen_topology(resultsdir = '.', jobid = None, outputobj = sys.stdout, format 
                 if core < len(res) and res[core] > 0:
                   utilization = results['network.shmem-1.mesh.link-%s.total-time-used' % link][core] / float(time0)
                   color = util2color(utilization)
-                  paint_box(((x + (.5 + _x)*.8 - BOXSIZE/2.)*SCALE_X, y + (.5 + _y)*.8 - BOXSIZE/2. + OFFSET_Y), (BOXSIZE*SCALE_X, BOXSIZE), 'link-%d-%d-%s' % (x, y, link), color = color, zorder = -1, margin = (0, 0))
+                  svg.paint_box(((x + (.5 + _x)*.8 - BOXSIZE/2.)*SCALE_X, y + (.5 + _y)*.8 - BOXSIZE/2. + OFFSET_Y), (BOXSIZE*SCALE_X, BOXSIZE), 'link-%d-%d-%s' % (x, y, link), color = color, zorder = -1, margin = (0, 0))
               if results['dram-queue.num-requests'][core] > 0:
                 utilization = results['dram-queue.total-time-used'][core] / float(time0)
                 color = util2color(utilization)
                 _x, _y = 0, .15
-                paint_box(((x + (.5 + _x)*.8 - 1.5*BOXSIZE/2.)*SCALE_X, y + (.5 + _y)*.8 - BOXSIZE/2. + OFFSET_Y), (1.5*BOXSIZE*SCALE_X, BOXSIZE), 'link-%d-%d-%s' % (x, y, link), color = color, zorder = -1, margin = (0, 0))
+                svg.paint_box(((x + (.5 + _x)*.8 - 1.5*BOXSIZE/2.)*SCALE_X, y + (.5 + _y)*.8 - BOXSIZE/2. + OFFSET_Y), (1.5*BOXSIZE*SCALE_X, BOXSIZE), 'link-%d-%d-%s' % (x, y, link), color = color, zorder = -1, margin = (0, 0))
 
 
-    paint_fini()
+    svg.write(outputobj)
+
 
 
 if __name__ == '__main__':
