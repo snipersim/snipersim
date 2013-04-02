949479d7649eb4a32af312af5cf9a113c2ac71cb
Wim Heirman <wim@heirman.net>
2013-04-02 11:43:39 +0200

    [cache_set_lru] Due to invalidations, we may not always find a way with lru_bits == associativity-1, so just always pick the way with the highest LRU bits

--- common/core/memory_subsystem/cache/cache_set_lru.cc
+++ common/core/memory_subsystem/cache/cache_set_lru.cc
@@ -16,18 +16,25 @@ CacheSetLRU::~CacheSetLRU()
    delete [] m_lru_bits;
 }
 
-UInt32 
+UInt32
 CacheSetLRU::getReplacementIndex()
 {
-   // Invalidations may mess up the LRU bits
-   UInt32 index = m_associativity;
+   // Invalidations may mess up the LRU bits, so find either a free way or the one with the highest LRU bits
+   UInt32 index = 0;
+   UInt8 max_bits = 0;
    for (UInt32 i = 0; i < m_associativity; i++)
    {
       if (!m_cache_block_info_array[i]->isValid())
+      {
          return i;
-      else if (m_lru_bits[i] == (m_associativity-1))
+      }
+      else if (m_lru_bits[i] > max_bits)
+      {
          index = i;
+         max_bits = m_lru_bits[i];
+      }
    }
+
    LOG_ASSERT_ERROR(index < m_associativity, "Error Finding LRU bits");
    return index;
 }
