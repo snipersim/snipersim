631133ab1761378aa88c93c30614bfbb7f9bdb0b
Wim Heirman <wim@heirman.net>
2013-04-18 09:18:09 +0200

    [scripts] Update dvfs.py script to use colon, not comma, to separate arguments as commas interfere with heterogeneous configuration parsing

--- scripts/dvfs.py
+++ scripts/dvfs.py
@@ -4,7 +4,7 @@ dvfs.py
 Change core frequencies according to a predifined list
 Argument is a list of time,core,frequency values. Time is in nanoseconds, frequency in MHz
 Example:
--sdvfs:1000,1,3000,2500,1,2660
+-sdvfs:1000:1:3000:2500:1:2660
 Change core 1 to 3 GHz after 1 us, change core 1 to 2.66 GHz after 2.5 us
 """
 
@@ -13,7 +13,7 @@ import sys, os, sim
 class Dvfs:
   def setup(self, args):
     self.events = []
-    args = args.split(',')
+    args = args.split(':')
     for i in range(0, len(args), 3):
       self.events.append((long(args[i])*sim.util.Time.NS, int(args[i+1]), int(args[i+2])))
     self.events.sort()
