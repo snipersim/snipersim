25537e0e9a49899efa39ac099480413c77256fa9
Trevor Erik Carlson <Trevor.Carlson@elis.UGent.be>
2013-06-16 01:13:00 +0200

    [scripting] Add additional config accessor methods to allow for explicit conversion. This is needed for boolean values, as the string 'false' needs to be a Python False

--- common/scripting/py_config.cc
+++ common/scripting/py_config.cc
@@ -20,9 +20,66 @@ getConfigString(PyObject *self, PyObject *args)
    return PyString_FromString(result.c_str());
 }
 
+static PyObject *
+getConfigInt(PyObject *self, PyObject *args)
+{
+   const char *key = NULL;
+   SInt64 index = -1;
+   SInt64 result = 0;
+
+   if (!PyArg_ParseTuple(args, "s|i", &key, &index))
+      return NULL;
+
+   if (index != -1)
+      result = Sim()->getCfg()->getIntArray(key, index);
+   else
+      result = Sim()->getCfg()->getInt(key);
+
+   return PyLong_FromLongLong(result);
+}
+
+static PyObject *
+getConfigFloat(PyObject *self, PyObject *args)
+{
+   const char *key = NULL;
+   SInt64 index = -1;
+   double result = 0;
+
+   if (!PyArg_ParseTuple(args, "s|i", &key, &index))
+      return NULL;
+
+   if (index != -1)
+      result = Sim()->getCfg()->getFloatArray(key, index);
+   else
+      result = Sim()->getCfg()->getFloat(key);
+
+   return PyFloat_FromDouble(result);
+}
+
+static PyObject *
+getConfigBool(PyObject *self, PyObject *args)
+{
+   const char *key = NULL;
+   SInt64 index = -1;
+   bool result = false;
+
+   if (!PyArg_ParseTuple(args, "s|i", &key, &index))
+      return NULL;
+
+   if (index != -1)
+      result = Sim()->getCfg()->getBoolArray(key, index);
+   else
+      result = Sim()->getCfg()->getBool(key);
+
+   return PyBool_FromLong(result);
+}
+
 
 static PyMethodDef PyConfigMethods[] = {
    {"get",  getConfigString, METH_VARARGS, "Get configuration variable."},
+   {"get_int",  getConfigInt, METH_VARARGS, "Get configuration variable."},
+   {"get_float",  getConfigFloat, METH_VARARGS, "Get configuration variable."},
+   {"get_bool",  getConfigBool, METH_VARARGS, "Get configuration variable."},
    {NULL, NULL, 0, NULL} /* Sentinel */
 };
 
