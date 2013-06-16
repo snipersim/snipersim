7fd68de15f0acccf0a07d542626b017270ca6a1a
Trevor Erik Carlson <Trevor.Carlson@elis.UGent.be>
2013-06-16 16:54:35 +0200

    [viz] Support flexible column data in the routine tracer data

--- tools/viz/functionparser.py
+++ tools/viz/functionparser.py
@@ -6,7 +6,7 @@ import subprocess
 
 #format of inputdata:
 
-#example : eip  name	source	calls	bits_used	bits_total	instruction_count	core_elapsed_time	nonidle_elapsed_time	fp_addsub	fp_muldiv	l3miss	global_instructions	global_nonidle_elapsed_time	cpiBase	cpiBranchPredictor	cpiMem	ADDPD	ADDSD	ADDSS	ADDPS	SUBPD	SUBSD	SUBSS	SUBPS	MULPD	MULSD	MULSS	MULPS	DIVPD	DIVSD	DIVSS	DIVPS
+#example : eip  name	source	calls	bits_used	bits_total	instruction_count	core_elapsed_time	nonidle_elapsed_time	fp_addsub	fp_muldiv	l2miss	l3miss	global_instructions	global_nonidle_elapsed_time	cpiBase	cpiBranchPredictor	cpiMem	ADDPD	ADDSD	ADDSS	ADDPS	SUBPD	SUBSD	SUBSS	SUBPS	MULPD	MULSD	MULSS	MULPS	DIVPD	DIVSD	DIVSS	DIVPS
 
 #Demangle C++ names:
 
@@ -33,57 +33,61 @@ def parseFunctions(inputfile):
   f=open(inputfile, "r" )
   i=0
   idnr=0
+  name2idx={}
   for line in f:
     file=line  
-    output=file.split('\t')
+    output=file.rstrip(' \n').split('\t')
+    if i == 0:
+      name2idx = dict(map(lambda a:(a[1],a[0]), enumerate(output)))
     #only functions that execute 1 or more instructions are stored
     if i > 0 and float(output[6]) >0:
       functiondata.append(dict(
-	eip				=output[0], 
-	name				=output[1],
-        name_clean			=cppfilt(output[1]), 
-	source				=output[2], 
-	calls				=float(output[3]),
-        bits_used			=float(output[4]),
-        bits_total			=float(output[5]),
-	instruction_count		=float(output[6]), 
-	core_elapsed_time		=float(output[7]),
-        nonidle_elapsed_time 		=float(output[8]),
-	fp_addsub			=float(output[9]),
-        fp_muldiv			=float(output[10]), 
-	l3miss				=float(output[11]),
-        global_instructions		=float(output[12]),
-        global_non_idle_elapsed_time	=float(output[13]),
-        cpiBase				=float(output[14]),
-        cpiBranchPredictor		=float(output[15]),
-	cpiMem				=float(output[16]),
-	addpd				=float(output[17]),
-	addsd				=float(output[18]),
-	addss				=float(output[19]),
-	addps				=float(output[20]),
-	subpd                           =float(output[21]),
-        subsd                           =float(output[22]),
-        subss                           =float(output[23]),
-        subps                           =float(output[24]),
-	mulpd                           =float(output[25]),
-        mulsd                           =float(output[26]),
-        mulss                           =float(output[27]),
-        mulps                           =float(output[28]),
-	divpd                           =float(output[29]),
-        divsd                           =float(output[30]),
-        divss                           =float(output[31]),
-        divps                           =float(output[32]),
+	eip				=output[name2idx['eip']], 
+	name				=output[name2idx['name']],
+        name_clean			=cppfilt(output[name2idx['name']]), 
+	source				=output[name2idx['source']], 
+	calls				=float(output[name2idx['calls']]),
+        bits_used			=float(output[name2idx['bits_used']]),
+        bits_total			=float(output[name2idx['bits_total']]),
+	instruction_count		=float(output[name2idx['instruction_count']]), 
+	core_elapsed_time		=float(output[name2idx['core_elapsed_time']]),
+        nonidle_elapsed_time 		=float(output[name2idx['nonidle_elapsed_time']]),
+	fp_addsub			=float(output[name2idx['fp_addsub']]),
+        fp_muldiv			=float(output[name2idx['fp_muldiv']]), 
+	l2miss				=float(output[name2idx['l2miss']]),
+	l3miss				=float(output[name2idx['l3miss']]),
+        global_instructions		=float(output[name2idx['global_instructions']]),
+        global_non_idle_elapsed_time	=float(output[name2idx['global_nonidle_elapsed_time']]),
+        cpiBase				=float(output[name2idx['cpiBase']]),
+        cpiBranchPredictor		=float(output[name2idx['cpiBranchPredictor']]),
+	cpiMem				=float(output[name2idx['cpiMem']]),
+	addpd				=float(output[name2idx['ADDPD']]),
+	addsd				=float(output[name2idx['ADDSD']]),
+	addss				=float(output[name2idx['ADDSS']]),
+	addps				=float(output[name2idx['ADDPS']]),
+	subpd                           =float(output[name2idx['SUBPD']]),
+        subsd                           =float(output[name2idx['SUBSD']]),
+        subss                           =float(output[name2idx['SUBSS']]),
+        subps                           =float(output[name2idx['SUBPS']]),
+	mulpd                           =float(output[name2idx['MULPD']]),
+        mulsd                           =float(output[name2idx['MULSD']]),
+        mulss                           =float(output[name2idx['MULSS']]),
+        mulps                           =float(output[name2idx['MULPS']]),
+	divpd                           =float(output[name2idx['DIVPD']]),
+        divsd                           =float(output[name2idx['DIVSD']]),
+        divss                           =float(output[name2idx['DIVSS']]),
+        divps                           =float(output[name2idx['DIVPS']]),
 	time_won_back			=0,
 	optimizations			=[],
         id				=idnr
 	))
-      total["calls"]+=float(output[3])
-      total["instruction_count"]+=float(output[6])
-      total["core_elapsed_time"]+=(float(output[7]))
-      total["nonidle_elapsed_time"]+=(float(output[8]))
-      total["fp_addsub"]+=float(output[9])
-      total["fp_muldiv"]+=float(output[10])
-      total["l3miss"]+=float(output[11])
+      total["calls"]+=float(output[name2idx['calls']])
+      total["instruction_count"]+=float(output[name2idx['instruction_count']])
+      total["core_elapsed_time"]+=(float(output[name2idx['core_elapsed_time']]))
+      total["nonidle_elapsed_time"]+=(float(output[name2idx['nonidle_elapsed_time']]))
+      total["fp_addsub"]+=float(output[name2idx['fp_addsub']])
+      total["fp_muldiv"]+=float(output[name2idx['fp_muldiv']])
+      total["l3miss"]+=float(output[name2idx['l3miss']])
 
       idnr+=1
     i+=1
