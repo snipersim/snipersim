c913c86ec7d6b3c44d3fc113f4ddde7af813d881
Wim Heirman <wim@heirman.net>
2013-07-05 22:36:11 +0200

    [progresstrace] Include simulated global IPC in progress report

--- scripts/progresstrace.py
+++ scripts/progresstrace.py
@@ -5,6 +5,11 @@ class ProgressTrace:
     self.last_ins = 0
     self.last_time = 0
     sim.util.EveryIns(1000000, self.periodic, roi_only = False)
+    self.sd = sim.util.StatsDelta()
+    self.stats = {
+      'time': self.sd.getter('barrier', 0, 'global_time'),
+      'instrs': [ self.sd.getter('core', core, 'instructions') for core in range(sim.config.ncores) ],
+    }
 
   def periodic(self, ins, ins_delta):
     time_now = time.time()
@@ -12,7 +17,14 @@ class ProgressTrace:
     if time_delta >= 1:
       ins_delta = ins - self.last_ins
       ips = ins_delta / time_delta
-      print '[PROGRESS] %.0fM instructions, %.0f KIPS' % (ins / 1e6, ips / 1e3)
+      self.sd.update()
+      if self.stats['time'].delta:
+        cycles = self.stats['time'].delta * sim.dvfs.get_frequency(0) / 1e9 # convert fs to cycles
+        instrs = sum([ self.stats['instrs'][core].delta for core in range(sim.config.ncores) ])
+        ipc = instrs / (cycles or 1)
+      else:
+        ipc = None
+      print ('[PROGRESS] %.0fM instructions, %.0f KIPS' % (ins / 1e6, ips / 1e3)) + (', %.2f IPC' % ipc if ipc else '')
       self.last_ins = ins
       self.last_time = time_now
 
