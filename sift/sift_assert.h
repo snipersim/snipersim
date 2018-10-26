28b54f9e5d37f6e83d4ca3bae4ecafd8897b5e21
Trevor E. Carlson <tcarlson@comp.nus.edu.sg>
2018-10-26 22:19:00 +0800

    [sift_assert] Check for the definition of __ASSERT_FUNCTION before trying to define it

--- sift/sift_assert.h
+++ sift/sift_assert.h
@@ -17,7 +17,8 @@
 	#endif
 #endif
 
-#if defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
+#if defined __ASSERT_FUNCTION
+#elif defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
 	#define __ASSERT_FUNCTION	__PRETTY_FUNCTION__
 #else
 	#if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
--- sift/standalone_pin3.0/libsift/sift_assert.h
+++ sift/standalone_pin3.0/libsift/sift_assert.h
@@ -15,7 +15,8 @@
 	#define __THROW
 #endif
 
-#if defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
+#if defined __ASSERT_FUNCTION
+#elif defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
 	#define __ASSERT_FUNCTION	__PRETTY_FUNCTION__
 #else
 	#if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
