5789d63682c3f8995c3384482ec7ef00c4b35146
Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
2023-07-18 18:57:48 +0900

    [tools] Do not hardcode absolute paths to dependencies
    tools/checkdependencies.py used to have some possible paths to
    dependencies hardcoded, but this approach is inherently limited as the
    user may install dependencies into arbitrary paths. Sometimes it is not
    possible to install dependencies to a well-known path because of file
    permissions.
    Instead, let the platform resolve paths for dependencies as other build
    systems typically do.

--- tools/checkdependencies.py
+++ tools/checkdependencies.py
@@ -1,53 +1,51 @@
 #!/usr/bin/env python2
 
-import sys, os
+import subprocess, sys, os
 
 # list of (packagename, filename)
 
-DEPENDENCIES = [
-  ('zlib1g-dev / zlib-devel', '/usr/include/zlib.h'),
-  ('libbz2-dev / bzip2-devel', '/usr/include/bzlib.h'),
-  ('g++ / gcc-c++', '/usr/bin/g++'),
-  ('wget', '/usr/bin/wget'),
+EXECUTABLE_DEPENDENCIES = [
+  ('g++ / gcc-c++', 'g++'),
+  ('wget', 'wget'),
 ]
 
-if os.environ.get('BOOST_INCLUDE', ''):
-  DEPENDENCIES += [
-    ('boost headers to %s' % os.environ['BOOST_INCLUDE'], '%(BOOST_INCLUDE)s/boost/lexical_cast.hpp' % os.environ),
-  ]
-else:
-  DEPENDENCIES += [
-    ('libboost-dev / boost-devel', '/usr/include/boost/lexical_cast.hpp'),
-  ]
-
-if os.environ.get('SQLITE_PATH', ''):
-  DEPENDENCIES += [
-    ('sqlite to %s' % os.environ['SQLITE_PATH'], '%(SQLITE_PATH)s/include/sqlite3.h' % os.environ),
-  ]
-else:
-  DEPENDENCIES += [
-    ('libsqlite3-dev / sqlite-devel', '/usr/include/sqlite3.h'),
-  ]
-
-ALTERNATIVES = [
-  ('/usr/lib', '/usr/lib/x86_64-linux-gnu'),
-  ('/usr/lib', '/usr/lib64'),
+HEADER_DEPENDENCIES = [
+  ('zlib1g-dev / zlib-devel', 'zlib.h'),
+  ('libbz2-dev / bzip2-devel', 'bzlib.h'),
+  ('libboost-dev / boost-devel', 'boost/lexical_cast.hpp'),
+  ('libsqlite3-dev / sqlite-devel', 'sqlite3.h'),
 ]
 
 missing = False
 
-def find_file(filename):
-  if os.path.exists(filename):
-    return True
-  for pattern, replacement in ALTERNATIVES:
-    if os.path.exists(filename.replace(pattern, replacement)):
-      return True
-  return False
-
-for package, filename in DEPENDENCIES:
-  if not find_file(filename):
+for package, filename in EXECUTABLE_DEPENDENCIES:
+  try:
+    with open(os.devnull) as devnull:
+      subprocess.call(filename, stdout=devnull, stderr=devnull)
+  except:
     print '*** Please install package %s' % package
     missing = True
+
+compiler_args = ['g++', '-x', 'c++', '-E', '-']
+
+if not missing:
+  boost_include = os.environ.get('BOOST_INCLUDE', None)
+  sqlite_path = os.environ.get('SQLITE_PATH', None)
+
+  if boost_include:
+    compiler_args.append('-I%s' % boost_include)
+
+  if sqlite_path:
+    compiler_args.append('-I%s/include' % sqlite_path)
+
+with open(os.devnull, 'w') as devnull:
+  for package, filename in HEADER_DEPENDENCIES:
+    popen = subprocess.Popen(compiler_args, stdin=subprocess.PIPE, stdout=devnull, stderr=devnull)
+    popen.stdin.write('#include <%s>' % filename)
+    popen.stdin.close()
+    if popen.wait():
+      print ('*** Please install package %s' % package)
+      missing = True
 
 
 if missing:
