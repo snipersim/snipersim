f5321e56f40ec6643f66e2fae88d27bf4d392763
JaimeRoelandts <jaimeroelandts@gmail.com>
2024-09-05 15:55:36 +0200

    Support for c++20, updated XED and PIN (not PINPLAY)

--- common/scripting/py_stats.cc
+++ common/scripting/py_stats.cc
@@ -48,54 +48,12 @@ statsGetterGet(PyObject *self, PyObject *args, PyObject *kw)
 }
 
 static PyTypeObject statsGetterType = {
-   PyObject_HEAD_INIT(NULL)
-   0,                         /*ob_size*/
-   "statsGetter",             /*tp_name*/
-   sizeof(statsGetterObject), /*tp_basicsize*/
-   0,                         /*tp_itemsize*/
-   0,                         /*tp_dealloc*/
-   0,                         /*tp_print*/
-   0,                         /*tp_getattr*/
-   0,                         /*tp_setattr*/
-   0,                         /*tp_compare*/
-   0,                         /*tp_repr*/
-   0,                         /*tp_as_number*/
-   0,                         /*tp_as_sequence*/
-   0,                         /*tp_as_mapping*/
-   0,                         /*tp_hash */
-   statsGetterGet,            /*tp_call*/
-   0,                         /*tp_str*/
-   0,                         /*tp_getattro*/
-   0,                         /*tp_setattro*/
-   0,                         /*tp_as_buffer*/
-   Py_TPFLAGS_DEFAULT,        /*tp_flags*/
-   "Stats getter objects",    /*tp_doc*/
-   0,                         /*tp_traverse*/
-   0,                         /*tp_clear*/
-   0,                         /*tp_richcompare*/
-   0,                         /*tp_weaklistoffset*/
-   0,                         /*tp_iter*/
-   0,                         /*tp_iternext*/
-   0,                         /*tp_methods*/
-   0,                         /*tp_members*/
-   0,                         /*tp_getset*/
-   0,                         /*tp_base*/
-   0,                         /*tp_dict*/
-   0,                         /*tp_descr_get*/
-   0,                         /*tp_descr_set*/
-   0,                         /*tp_dictoffset*/
-   0,                         /*tp_init*/
-   0,                         /*tp_alloc*/
-   0,                         /*tp_new*/
-   0,                         /*tp_free*/
-   0,                         /*tp_is_gc*/
-   0,                         /*tp_bases*/
-   0,                         /*tp_mro*/
-   0,                         /*tp_cache*/
-   0,                         /*tp_subclasses*/
-   0,                         /*tp_weaklist*/
-   0,                         /*tp_del*/
-   0,                         /*tp_version_tag*/
+   .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
+   .tp_name = "statsGetter",
+   .tp_basicsize = sizeof(statsGetterObject),
+   .tp_call = statsGetterGet,
+   .tp_flags = Py_TPFLAGS_DEFAULT,
+   .tp_doc = PyDoc_STR("Stats getter objects"),
 };
 
 static PyObject *
